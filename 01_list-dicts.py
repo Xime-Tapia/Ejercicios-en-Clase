f25ace99c38c1ff0bb17546a4f8e342105086ee3
Xime-Tapia <59945827+Xime-Tapia@users.noreply.github.com>
2020-01-17 14:14:35 -0500

    EJERCICIOS EN CLASE CURSO PYTHON PARA INGENIERIA CEC

---
+++ 01_list-dicts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 11:35:21 2020
+
+@author: jxtmy
+"""
+
+print ("HELLO WORLD")
---
+++ 04_list-dicts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 12:21:47 2020
+
+@author: CEC
+"""
+
+nativevlan=100
+datavlan=800
+if nativevlan == datavlan:
+    print("The native VLAN and the data VLAN are the same.")
+else:
+    print("The native VLAN and the data VLAN are different.")
